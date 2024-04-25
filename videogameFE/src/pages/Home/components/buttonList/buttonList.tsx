62450162ac11d924c3d701c52fba3ea7ea10f02f
Guadalupe <guadalupe.miranda997@gmail.com>
2024-04-25 18:50:36 -0300

    - Agregada reedirección en el home y un poquito de css

--- videogameFE/src/pages/Home/components/buttonList/buttonList.tsx
+++ videogameFE/src/pages/Home/components/buttonList/buttonList.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom"
 import "./buttonList.css"
 
 const ButtonList = () => {
     return(
-        <div className="buttonListContainer">
+        <Link to={"/myList"} className="buttonListContainer">
             Explora tus listados
-        </div>
+        </Link>
     )
 }
 
