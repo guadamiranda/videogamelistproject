00cb5582f98d8d468d6667b2e5e0c6cb43ae0dae
Guadalupe <guadalupe.miranda997@gmail.com>
2024-04-29 17:26:42 -0300

    Terminado el endpoint para crear favoritos. Hay que probar el get y el post :)

--- videogameBE/express/src/infraestructure/repository/favourites.ts
+++ videogameBE/express/src/infraestructure/repository/favourites.ts
@@ -1,12 +1,31 @@
 import model from '../repository/models/favorites'
 import { favouriteModel } from '../../domain/models/favourite'
 
+const buildFavouriteModel = (favouriteCreated: any): favouriteModel => {
+    return {
+        userId: favouriteCreated.userId,
+        gameId: favouriteCreated.gameId,
+        state: favouriteCreated.state,
+        note: favouriteCreated.note,
+        comment: favouriteCreated.comment
+    } as favouriteModel;
+  };
+
 const getFavouritesByListType = async(userId:string, listType:string):Promise<favouriteModel[]> => {
     return await model.find({ $and: [{ userId: userId }, { state: listType }] })
 }
 
 const createFavouriteFromDB = async(favorite: favouriteModel):Promise<favouriteModel> => {
-    
+    const favouriteCreated = await model.create(
+        {
+            userId: favorite.userId,
+            gameId: favorite.gameId,
+            state: favorite.state,
+            note: favorite.note,
+            comment: favorite.comment
+          });
+
+    return buildFavouriteModel(favouriteCreated);
 }
 
 export { getFavouritesByListType, createFavouriteFromDB }
